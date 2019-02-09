196e9e9b1f1de059fdcb057be688d37219a5580b
tommyod <tommy.odland@gmail.com>
2019-02-09 17:35:43 +0100

    changed travis file

--- statscollection/__init__.py
+++ statscollection/__init__.py
@@ -1,4 +1,4 @@
-#from statscollection import example
-#from statscollection import online
+# from statscollection import example
+# from statscollection import online
 
-__version__ = '0.0.1'
+__version__ = "0.0.1"

6d95294e3ef3025abbd74e6b94b90c99b1793b99
tommyod <tommy.odland@gmail.com>
2019-02-09 17:54:41 +0100

    added requirements

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     license="MIT",
     packages=find_packages(exclude=[]),
     python_requires=">=3.6",
+    install_requires=["numpy>=1.11.0", "scipy>=0.17.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",

3b0ce59d955ced9d18fb1dcec691826a4c8915a5
tommyod <tommy.odland@gmail.com>
2019-02-19 19:12:44 +0100

    added pandas requiremen

--- statscollection/resampling/__init__.py
+++ statscollection/resampling/__init__.py
@@ -12,11 +12,11 @@ Docs go here.
 >>> from statscollection.resampling import bootstrap_function
 >>> sleep = np.array([7.07, 6.99, 8.49, 7.67, 6.95, 7.64, 8.03])
 >>> medians = bootstrap_function(sleep, np.median, num_bootstraps=100, random_state=42)
->>> ans = np.quantile(medians, q=[0.05, 0.95], interpolation='linear')
+>>> ans = np.percentile(medians, q=[5, 95], interpolation='linear')
 >>> print(ans)
 [6.99... 8.03...]
 >>> means = bootstrap_function(sleep, np.mean, num_bootstraps=1000, random_state=42)
->>> ans = np.quantile(means, q=[0.01, 0.99], interpolation='linear')
+>>> ans = np.percentile(means, q=[1, 99], interpolation='linear')
 >>> print(ans)
 [7.13... 8.03...]
 
